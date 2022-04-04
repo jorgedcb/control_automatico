668d01da8bb7f4da0360cedf07f1cf040a4ff39f
jorgedcb <jecb2001@gmail.com>
2022-04-04 12:42:40 -0500

    lab 2

---
+++ punto 2/grafica2.m
@@ -0,0 +1,17 @@
+I= 25;
+KiKc = 348100;
+Ks=0.01;
+a1= KiKc/I;
+b1= (KiKc*Ks)/I;
+
+
+x=tf([1*a1 0.5*a1],[1 b1 a1]);
+x2=tf([2.2*a1 6*a1],[1 b1 a1]);
+x3=tf([0.2*a1 2*a1],[1 b1 a1]);
+
+hold on
+step (x);
+step (x2);
+Step (x3);
+
+hold off
